68b49971fe85b2128c331ca70e2208619c988a36
Natalia Ferreira <nataliaferreira.dev@yahoo.com.br>
2022-09-30 21:30:39 -0300

    Inclusao do Swagger

--- src/main/java/br/com/dio/parking/controller/HelloController.java
+++ src/main/java/br/com/dio/parking/controller/HelloController.java
@@ -1,9 +1,13 @@
 package br.com.dio.parking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
+@RequestMapping("/")
+@ApiIgnore
 public class HelloController {
 
     @GetMapping
--- src/main/java/br/com/dio/parking/controller/ParkingController.java
+++ src/main/java/br/com/dio/parking/controller/ParkingController.java
@@ -5,6 +5,8 @@ import br.com.dio.parking.model.dto.ParkingCreateDTO;
 import br.com.dio.parking.model.dto.ParkingDTO;
 import br.com.dio.parking.model.mapper.ParkingMapper;
 import br.com.dio.parking.service.ParkingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/parking")
+@Api(tags = "Parking Controller")
 public class ParkingController {
 
     private final ParkingService parkingService;
@@ -24,6 +27,7 @@ public class ParkingController {
     }
 
     @GetMapping
+    @ApiOperation("Find all parking")
     public ResponseEntity<List<ParkingDTO>>findAll(){
 
         List<Parking> parkingList = parkingService.findAll();
@@ -33,6 +37,7 @@ public class ParkingController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Find by id")
     public ResponseEntity<ParkingDTO>findById(@PathVariable String id){
 
         Parking parking = parkingService.findById(id);
@@ -42,6 +47,7 @@ public class ParkingController {
     }
 
     @PostMapping
+    @ApiOperation("Create parking")
     public ResponseEntity<ParkingDTO>create(@RequestBody ParkingCreateDTO dto){
         var parkingCreate = parkingMapper.toParkingCreate(dto);
         var parking = parkingService.create(parkingCreate);
